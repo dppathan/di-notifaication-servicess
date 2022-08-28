d4839b6cca6df160f9c583f3d1d003a8294c32da
dppathan <daudpathan8@gmail.com>
2022-08-28 16:09:11 +0530

    first ever commit

---
+++ di-notification-service/src/main/java/com/lti/colt/dis/DiNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lti.colt.dis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiNotificationServiceApplication.class, args);
+	}
+
+}
---
+++ di-notification-service/src/test/java/com/lti/colt/dis/DiNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lti.colt.dis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
